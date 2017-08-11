b2e8db9450f2a45d52748360dd0ccb4002211d43
VictorOrtiz0320 <30841617+VictorOrtiz0320@users.noreply.github.com>
2017-08-10 20:21:10 -0500

    actualizacion

--- P1/Practica1.R
+++ P1/Practica1.R
@@ -1,43 +1,50 @@
-repetir <- 1
-duracion <- 500
-library(parallel)
- 
-cluster <- makeCluster(detectCores() - 1)
-clusterExport(cluster, "duracion")
-datos <-  data.frame()
- 
-for (dimension in 1:2) {
-    clusterExport(cluster, "dimension")
-    resultado <- parSapply(cluster, 1:repetir,
-                           function(r) {
-						pos=rep(0,dimension)
-						n=length(pos)
-						n
-						origen=c(1,2)
-						m=length(origen)
-						m
-						contador=0 
-						for (t in 1:duracion) {
-      					cambiar <- sample(1:dimension, 1)
-        					cambio <- 1
-        					if (runif(1) < 0.5) {
-            				cambio <- -1
-        					}
-						pos[cambiar]= pos[cambiar]+ cambio
-						if (all (pos==origen)){
-						contador=contador +1 
-						}
-						else{
-						contador=contador
-						}}
-						return(contador)
-						})
-    datos <- rbind(datos, resultado)
+dur=10
+
+contador1=0
+contador2=0
+contador3=0 
+
+for (dim in 1:3) {
+	pos=rep(0,dim)
+	origen=c(pos)
+	contador=0
+
+	for (t in 1:dur) {
+        cambiar <- sample(1:dim, 1)
+        cambio <- 1
+        if (runif(1) < 0.5) {
+            cambio <- -1
+        }
+	pos[cambiar]= pos[cambiar]+ cambio
+   if (all (pos==origen)){
+	contador=contador +1 }
+   else{
+      contador=contador
 }
-stopCluster(cluster)
-    png("p1er.png")
-    boxplot(data.matrix(datos), use.cols=FALSE, 
-       xlab="Dimensi\u{F3}n", ylab="N. veces vuelve al origen", 
-       main="Practica 1")
- 
-graphics.off()
+print (pos)
+}
+print ("N. veces que vuelve al origen")
+print (contador)
+
+if (dim==1){
+	contador1=contador1+ contador
+}
+if(dim==2){
+	contador2=contador2+ contador
+}
+if (dim==3){
+	contador3=contador3+ contador
+}
+
+}
+contador1
+contador2
+contador3
+datos=data.frame()
+dimensiones=sort(sample(1:dim))
+norigen=c(contador1,contador2,contador3)
+datos=rbind(norigen,dimensiones)
+barplot(norigen,main="Practica 1", xlab="Dimensiones", ylab="N. veces vuelve al origen",names.arg=dimensiones)
+
+
+
