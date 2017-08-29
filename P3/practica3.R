a434b5d208dbc9b8398c1adddee0243437aff611
VictorOrtiz0320 <30841617+VictorOrtiz0320@users.noreply.github.com>
2017-08-29 01:03:03 -0500

    Practica3

--- P3/practica3.R
+++ P3/practica3.R
@@ -13,43 +13,48 @@ primo <- function(n) {
     }
     return(TRUE)
 }
+
 #Hacer un vector de puro numeros primos y mandarlos directamente
 suppressMessages(library(doParallel))
 datos=data.frame()
-Toriginal=data.frame() 
-Tinvertido=data.frame()
-Taleatorio=data.frame()
+
 desde <- 1000
-hasta <-  3000
+hasta <-  10000
 original <- desde:hasta
 invertido <- hasta:desde
-replicas <- 3
+pares = seq(desde+1, (desde + 1 + 2*(hasta-desde))/2,2)
+replicas <- 20
 MaxNucleos=detectCores()-1
-par(mfrow=c(1,nucleos))
+
 
 for (nucleos in 1:MaxNucleos){
-  
-  cluster = makeCluster(nucleos)
-
-
-  registerDoParallel(cluster)
+ 
+  registerDoParallel(makeCluster(nucleos))
   ot <-  numeric()
   it <-  numeric()
   at <-  numeric()
+  tp <-  numeric()
+  
   for (r in 1:replicas) {
     ot <- c(ot, system.time(foreach(n = original, .combine=c) %dopar% primo(n))[3]) # de menor a mayor
     it <- c(it, system.time(foreach(n = invertido, .combine=c) %dopar% primo(n))[3]) # de mayor a menor
     at <- c(at, system.time(foreach(n = sample(original), .combine=c) %dopar% primo(n))[3]) # orden aleatorio
+    tp=   c(tp, system.time(foreach(n = pares  , .combine=c) %dopar% primo(n))[3]) #números pares
   }
   stopImplicitCluster()
-  stopCluster(cluster)
-  datos=rbind(ot,it,at)
+
+  
+  datos=rbind(ot,it,at,tp)
+ 
+  
+ #Generar archivos de imagen de los gráficos
   
   nArchivo = paste("p2_t", nucleos, ".png", sep="")
-  NumeroNucleo = paste("Numero de nucleo", nucleos)
+  NumeroNucleo = paste("Núcleos usados:", nucleos)
   png(nArchivo)
   boxplot(data.matrix(datos),use.cols=FALSE, xlab="Tipos de ordenamiento", ylab="Tejeución", main=NumeroNucleo)
   graphics.off()
-  
+
 }
-boxplot(data.matrix(datos),use.cols=FALSE, xlab="Tipos de ordenamiento", ylab="Tejeución", main=NumeroNucleo)
+
+
