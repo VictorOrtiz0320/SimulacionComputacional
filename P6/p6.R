4fc99978f73f5befe1b260312ab118de76817136
VictorOrtiz0320 <30841617+VictorOrtiz0320@users.noreply.github.com>
2017-09-17 11:12:58 -0500

    avance

--- P6/p6.R
+++ P6/p6.R
@@ -1,8 +1,10 @@
+Tinicial=Sys.time()
 l <- 1.5
 n <- 50
 pi <- 0.05
 pr <- 0.02
 v <- l / 30
+n <- 50
 agentes <- data.frame(x = double(), y = double(), dx = double(), dy = double(), estado  = character())
 for (i in 1:n) {
     e <- "S"
@@ -14,53 +16,63 @@ for (i in 1:n) {
                                          estado = e))
     levels(agentes$estado) <- c("S", "I", "R")
 }
+ncontagios=rep(FALSE,n)
+contagiados<- function (){
+  contagios <- rep(FALSE, n)
+  for (i in 1:n) { # posibles contagios
+    a1 <- agentes[i, ]
+    if (a1$estado == "I") { # desde los infectados
+      
+      for (j in 1:n) {
+        if (!contagios[j]) { # aun sin contagio
+          
+          a2 <- agentes[j, ]
+          if (a2$estado == "S") { # hacia los susceptibles
+            
+            dx <- a1$x - a2$x
+            dy <- a1$y - a2$y
+            d <- sqrt(dx^2 + dy^2)
+            if (d < r) { # umbral
+              p <- (r - d) / r
+              if (runif(1) < p) {
+            
+                return( contagios[j] <- TRUE)
+              } else{ return(contagios[j] <- FALSE)
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 epidemia <- integer()
 r <- 0.1
 tmax <- 100
 digitos <- floor(log(tmax, 10)) + 1
+
+
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+
+
 for (tiempo in 1:tmax) {
     infectados <- dim(agentes[agentes$estado == "I",])[1]
     epidemia <- c(epidemia, infectados)
     if (infectados == 0) {
         break
     }
+    
+    #paralelizar contagiados 
+   
+    contagios=foreach(t=1:50) %dopar% contagiados()
 
 
-contagiados<- function (){
-    contagios <- rep(FALSE, n)
-    for (i in 1:n) { # posibles contagios
-        a1 <- agentes[i, ]
-        if (a1$estado == "I") { # desde los infectados
-            for (j in 1:n) {
-                if (!contagios[j]) { # aun sin contagio
-                    a2 <- agentes[j, ]
-                    if (a2$estado == "S") { # hacia los susceptibles
-                        dx <- a1$x - a2$x
-                        dy <- a1$y - a2$y
-                        d <- sqrt(dx^2 + dy^2)
-                        if (d < r) { # umbral
-                            p <- (r - d) / r
-                            if (runif(1) < p) {
-                               return( contagios[j] <- TRUE)
-                            } else return(FALSE)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-#paralelizar contagiados 
-suppressMessages(library(doParallel))
-registerDoParallel(makeCluster(detectCores() - 1))
-contagiios=foreach(t=1:n, combine=c) %dopar% contagiados()
-#contagios=as.data.frame.logical(contagios)
-stopImplicitCluster()
-
-    for (i in 1:n) { # movimientos y actualizaciones
+  
+  for (i in 1:n) { # movimientos y actualizaciones
         a <- agentes[i, ]
-        if (contagios[i]) {
+        if (contagios[i]=="TRUE") {
             a$estado <- "I"
         } else if (a$estado == "I") { # ya estaba infectado
             if (runif(1) < pr) {
@@ -70,41 +82,46 @@ stopImplicitCluster()
         a$x <- a$x + a$dx
         a$y <- a$y + a$dy
         if (a$x > l) {
-            a$x <- a$x - l
+          a$x <- a$x - l
         }
         if (a$y > l) {
-            a$y <- a$y - l
+          a$y <- a$y - l
         }
         if (a$x < 0) {
-            a$x <- a$x + l
+          a$x <- a$x + l
         }
         if (a$y < 0) {
-            a$y <- a$y + l
+          a$y <- a$y + l
         }
         agentes[i, ] <- a
-    }
+  }
     aS <- agentes[agentes$estado == "S",]
     aI <- agentes[agentes$estado == "I",]
     aR <- agentes[agentes$estado == "R",]
     tl <- paste(tiempo, "", sep="")
     while (nchar(tl) < digitos) {
-        tl <- paste("0", tl, sep="")
+      tl <- paste("0", tl, sep="")
     }
     salida <- paste("p6_t", tl, ".png", sep="")
     tiempo <- paste("Paso", tiempo)
     png(salida)
     plot(l, type="n", main=tiempo, xlim=c(0, l), ylim=c(0, l), xlab="x", ylab="y")
     if (dim(aS)[1] > 0) {
-        points(aS$x, aS$y, pch=15, col="chartreuse3", bg="chartreuse3")
+      points(aS$x, aS$y, pch=15, col="chartreuse3", bg="chartreuse3")
     }
     if (dim(aI)[1] > 0) {
-        points(aI$x, aI$y, pch=16, col="firebrick2", bg="firebrick2")
+      points(aI$x, aI$y, pch=16, col="firebrick2", bg="firebrick2")
     }
     if (dim(aR)[1] > 0) {
-        points(aR$x, aR$y, pch=17, col="goldenrod", bg="goldenrod")
+      points(aR$x, aR$y, pch=17, col="goldenrod", bg="goldenrod")
     }
     graphics.off()
 }
+stopImplicitCluster()
+
 png("p6e.png", width=600, height=300)
 plot(1:length(epidemia), 100 * epidemia / n, xlab="Tiempo", ylab="Porcentahe de infectados")
 graphics.off()
+Tfinal=Sys.time()
+print(Tfinal-Tinicial)
+
