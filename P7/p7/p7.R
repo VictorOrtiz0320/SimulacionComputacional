21c4932572a99ee193637d7ac6194603d91d80d4
VictorOrtiz0320 <30841617+VictorOrtiz0320@users.noreply.github.com>
2017-09-25 20:46:52 -0500

    final

--- P7/p7/p7.R
+++ P7/p7/p7.R
@@ -6,14 +6,12 @@ f <- function(x, y) {
   return(((x + 0.5)^4 - 30 * x^2 - 20 * x + (y + 0.5)^4 - 30 * y^2 - 20 * y)/100)
 }
 
-
 low <- -2
 high <- 3
 step <- 0.25
-replicas <- 3
+replicas <- 10
 best=c()
-t=50
-k=1
+t=40
 wolfram=0.0666822
 
 BusquedaLocal <- function() {
@@ -45,7 +43,7 @@ BusquedaLocal <- function() {
     } else {
       besty <- c(currx,bot)
     }
-    ### Nuevo ###
+  
     if (f(bestx[1],bestx[2])> f(besty[1],besty[2]))
     {  currx=bestx[1]
        curry=bestx[2]
@@ -68,21 +66,37 @@ BusquedaLocal <- function() {
 suppressMessages(library(doParallel))
 registerDoParallel(makeCluster(detectCores() - 1))
 caminatas <- foreach(q=1:replicas, .combine=rbind) %dopar% BusquedaLocal()
+stopImplicitCluster()
 
 names(caminatas)=c("Replicas","Paso","x","y","f(x,y)")
 caminatas$Replicas=as.factor(caminatas$Replicas)
-stopImplicitCluster()
-R1=caminatas[caminatas$Replicas==1,]
-R2=caminatas[caminatas$Replicas==2,]
-R3=caminatas[caminatas$Replicas==3,]
-R4=caminatas[caminatas$Replicas==4,]
-R5=caminatas[caminatas$Replicas==5,]
 
-png("p7.png")
-#xyplot(data=R1,f(x,y)~Paso,pch = 16, col = "red")
-#xyplot(data=R2,f(x,y)~Paso,pch = 12, col = "blue")
-plot(data=caminatas,f(x,y)~Paso,type="p")
-#points(data=R1,R1$f(x,y), R1$Paso, pch=15, col="red")
-points(data=caminatas,f(x,y)~Paso,pch=20,col="blue")
-abline(h=wolfram,col="green",lwd=2)
+
+
+#Graficar
+
+png("p7.png",width = 500,height = 300)
+xyplot(data=caminatas,f(x,y)~Paso,groups = Replicas,
+       panel=function(x,y,subscripts,groups){
+         panel.grid(h=-1,v=-1)
+         panel.xyplot(x,y)
+         panel.stripplot(x,y,
+                         groups = groups, subscripts = subscripts,pch=19,type="o")
+         panel.abline(h=wolfram,col="Green",lwd=2)
+       })
 graphics.off()
+
+
+png("p7t.png",width = 500,height = 500)
+xyplot(data=caminatas,f(x,y)~Paso |Replicas,groups = Replicas,
+       panel=function(x,y,subscripts,groups){
+         panel.grid(h=-1,v=-1)
+         panel.xyplot(x,y)
+         panel.stripplot(x,y,
+                         groups = groups, subscripts = subscripts,pch=19,type="o")
+         panel.abline(h=wolfram,col="Green",lwd=2)
+       })
+graphics.off()
+
+
+
--- P7/reto2/reto2.R
+++ P7/reto2/reto2.R
@@ -1,6 +1,7 @@
 f <- function(x) {
   return(cos(14.5*x - 0.3) + x * (x + 0.2) + 1.01)
 }
+
 low <- -3
 high <- -low
 xg <- seq(low, high, 0.05)
@@ -8,7 +9,7 @@ y <- f(xg)
 tmax <- 100
 digitos <- floor(log(tmax, 10)) + 1
 step <- 0.3
-Temp=500
+Temp=1000
 e=0.995
 d=0
 
@@ -16,7 +17,7 @@ x <- runif(1, low, high)
 best=x
 #############################################################################
 for (tiempo in 1:tmax) {
- 
+
   valor=runif(1)
   prob=exp(-d/Temp)
   delta <- runif(1, -step, step)
@@ -52,7 +53,7 @@ for (tiempo in 1:tmax) {
   #if (f(curr) < f(best)) { # minimizamos
     #best <- curr
   #}
-  #############################################################################
+#############################################################################
   tl <- paste(tiempo, "", sep="")
   while (nchar(tl) < digitos) {
     tl <- paste("0", tl, sep="")
@@ -60,7 +61,7 @@ for (tiempo in 1:tmax) {
   salida <- paste("p7_t", tl, ".png", sep="")
   tiempo <- paste("Paso", tiempo)
   png(salida, width=500, height=400)
-  plot(xg, y, type="l")
+  plot(xg, y, type="l",xlab="x",ylab="F(x)")
   abline(v = best, col="green", lwd=2)
   points(x, f(x), pch=16, col="red")
   graphics.off()
