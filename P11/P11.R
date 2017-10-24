5320fb5a05f1e57ceeed9da3da98d34a8bd44707
VictorOrtiz0320 <30841617+VictorOrtiz0320@users.noreply.github.com>
2017-10-23 22:33:36 -0500

    final

--- P11/P11.R
+++ P11/P11.R
@@ -1,4 +1,5 @@
-
+#suppressMessages(library(doParallel))
+#registerDoParallel(makeCluster(detectCores() - 1))
 Tinicial=Sys.time()
 pick.one <- function(x) {
   if (length(x) == 1) {
@@ -40,73 +41,42 @@ tc <- 5
 #k <- 2 # cuantas funciones objetivo
 
 Fobjetivo<- function(i){ 
-
-return(poli(md,vc, tc))
+  
+  return(poli(md,vc, tc))
 }
 obj<-foreach(i=1:k) %dopar% Fobjetivo(i)
 minim <- (runif(k) > 0.5)
 sign <- (1 + -2 * minim)
-#n <- 100 # cuantas soluciones aleatorias
+#n <- 200 # cuantas soluciones aleatorias
 
 
 Fsoluciones<- function(i){ # evaluamos las soluciones
+  resul<-double()
+  for (j in 1:k) { # para todos los objetivos
+    datos<-eval(obj[[j]], sol[i,], tc)
+    resul<-cbind(resul,datos)
+  }
+  return(resul)
+}
 sol <- matrix(runif(vc * n), nrow=n, ncol=vc)
 val <- matrix(rep(NA, k * n), nrow=n, ncol=k)
-  for (j in 1:k) { # para todos los objetivos
-  return (eval(obj[[j]], sol[i,], tc))
-  }
-}
-val<-matrix(foreach(i=1:n, .combine = rbind) %dopar% Fsoluciones(i),nrow = n,ncol = k,byrow = TRUE)
+val<-foreach(i=1:n, .combine = rbind) %dopar% Fsoluciones(i)
 
-mejor1 <- which.max(sign[1] * val[,1])
-#mejor2 <- which.max(sign[2] * val[,2])
-cual <- c("max", "min")
-xl <- paste("Primer objetivo (", cual[minim[1] + 1], ")", sep="")
-yl <- paste("Segundo objetivo (", cual[minim[2] + 1], ")", sep="")
-###########################################################################################
-#png("p11_init.png")
-#plot(val[,1], val[,2], xlab=xl, ylab=yl, main="Ejemplo bidimensional")
-#graphics.off()
-#png("p11_mejores.png")
-#plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
-     #ylab=paste(yl,"mejor con bolita naranja"),
-     #main="Ejemplo bidimensional")
-#points(val[mejor1, 1], val[mejor1, 2], col="blue", pch=15, cex=1.5)
-#points(val[mejor2, 1], val[mejor2, 2], col="orange", pch=16, cex=1.5)
-#graphics.off()
-#############################################################################################
 Fpareto<- function(i){
-no.dom <- logical()
-dominadores <- integer()
+  no.dom <- logical()
+  dominadores <- integer()
   d <- logical()
   for (j in 1:n) {
     d <- c(d, domin.by(sign * val[i,], sign * val[j,], k))
   }
   cuantos <- sum(d)
-  return(dominadores <- c(dominadores, cuantos))
+  return(cuantos==0)
 }
-dominadores <-foreach(i=1:n, .combine = rbind) %dopar% Fpareto(i)
 
-#############################################################################################
-
-#GRAFICAR
-#png("p11_frente.png")
-#plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
-     #ylab=paste(yl,"mejor con bolita naranja"),
-     #main="Ejemplo bidimensional")
-#points(frente[,1], frente[,2], col="green", pch=16, cex=1.5)
-#graphics.off()
-
-#VIOLINPLOT
-#library(ggplot2) # recordar instalar si hace falta
-data <- data.frame(pos=rep(0, n), dom=dominadores)
-#png("p11_violin.png")
-gr <- ggplot(data, aes(x=pos, y=dom)) + geom_violin(fill="orange", color="red")
-gr + geom_boxplot(width=0.2, fill="blue", color="white", lwd=2) +
-  xlab("") +
-  ylab("Frecuencia") +
-  ggtitle("Cantidad de soluciones dominantes")
-#graphics.off()
+no.dom<-foreach(i=1:n, .combine = c) %dopar% Fpareto(i)
+frente<- subset(val, no.dom) # solamente las no dominadas
+tam<-dim(frente)[1]
+#stopImplicitCluster() 
 Tfinal=Sys.time()
 Tiempo=Tfinal-Tinicial
 print(Tiempo)
--- P11/P11V.R
+++ P11/P11V.R
@@ -1,28 +1,26 @@
 suppressMessages(library(doParallel))
 registerDoParallel(makeCluster(detectCores() - 1))
-library(ggplot2) # recordar instalar si hace falta
+library(ggplot2)
 Resultados=data.frame()
 Toriginal=numeric()
-Final<- data.frame()
-Resul<- data.frame()
-for (n in c(200)){
-  for (k in seq(2,8,1)){
-    
+n=200
+
+  for (k in seq(2,10,2)){
+    for (r in 1:50){
       
       source('~/GitHub/SimulacionComputacional/P11/P11.R', encoding = 'UTF-8')
-      Toriginal=cbind(dominadores,k,n)
+      Toriginal=cbind(tam,k,r)
       Resultados=rbind(Resultados,Toriginal)
   }
 }
 stopImplicitCluster() 
-
-names(Resultados)=c("Dominadores","nObjetivos","nSoluciones")
+names(Resultados)=c("Frente","nObjetivos","Replicas")
 Resultados$nObjetivos<- as.factor(Resultados$nObjetivos)
 library(ggplot2)
- ggplot(data=Resultados, aes(Resultados$nObjetivos, Resultados$Dominadores/n)) +
+ ggplot(data=Resultados, aes(Resultados$nObjetivos, (Resultados$Frente/n)*100)) +
   geom_violin(scale="width",fill="dodgerblue4", color="black")+
-   geom_boxplot(width=0.2, fill="dodgerblue2", color="aliceblue", lwd=2)+ 
+   geom_boxplot(width=0.2, fill="dodgerblue2", color="aliceblue")+ 
  xlab("Número de funciones objetivo k") +
- ylab("Porcentaje de funciones dominantes")
- #ggtitle("Cantidad de soluciones dominantes")
+ ylab("Porcentaje de funciones no dominantes (%)")+
+   theme_grey()
 ggsave(file=paste("p11_violin.png", sep='')) #Nombre del jpeg
