2b2a6e40de9a75ea4a1b29d598825bac1ad3bf25
VictorOrtiz0320 <30841617+VictorOrtiz0320@users.noreply.github.com>
2017-12-10 21:34:11 -0600

    avance

--- Proyecto/proyecto.R
+++ Proyecto/proyecto.R
@@ -1,8 +1,9 @@
 library(ggplot2)
 #Declaración del dataframe con las partículas iniciales
-l<-1.5
+l<-3
 velocidad<-l/60
-n <- 30 #Número de particulas 
+tabla=data.frame()
+n <- 20 #Número de particulas 
 particulas<- data.frame(posicion=numeric(),x = double(), y = double(), dx = double(), dy = double(), espesor=double(), estado=character(), lider=numeric())
 for(i in 1:n){
 particulas <-rbind(particulas, data.frame(x = runif(1, 0, l), y=runif(1, 0, l), c =runif(1, 5, 5.1), r=5, dx=runif(1,-velocidad,velocidad), dy=runif(1,-velocidad,velocidad), espesor=runif(1,0.01,0.1),estado="S",lider=0))
@@ -12,51 +13,30 @@ particulas$lider<-seq(1,n,1)
 levels(particulas$estado) <- c(levels(particulas$estado), "A","L")
 particulas$rf<-particulas$r+particulas$espesor
 matriz<-matrix(double(),nrow=n,ncol=n)
-umbral<-0.03 #Distancia de umbral de interacción
-dcc<-0.06 # Espesor de la doble capa electrica de interacción
+umbral<-0.02 #Distancia de umbral de interacción
+dcc<-0.05 # Espesor de la doble capa electrica de interacción
 
-#Movimiento Browniano
-tmax<-40 #duración de la simulación
+
+tmax<-80 #duración de la simulación
 
 
 for (tiempo in 1:tmax){
-  
-  
-  for(i in 1:n){
-    p<-particulas[i,]
-    
-    p$x<- p$x + p$dx
-    p$y<- p$y + p$dy
-    
-    if (p$x > l) {
-      p$x <- p$x - l
-    }
-    if (p$y > l) {
-      p$y <- p$y - l
-    }
-    if (p$x < 0) {
-      p$x <- p$x + l
-    }
-    if (p$y < 0) {
-      p$y <- p$y + l
-    }
-    particulas[i,]<-p
-    }
-  
 
 
- for (i in 1:n){
-   
+ for (i in seq(1,n-1,1)){
+  
    p1<- particulas[i,]
    if (p1$estado != "A"){
-     for (j in 1:n){
+     
+     for (j in seq(i+1,n,1)){
        p2<-particulas[j,]
        if (p2$estado != "A"){
-       dx<-p1$x-p2$x
-       dy<-p1$y-p2$y
+       dx1<-p1$x-p2$x
+       dy1<-p1$y-p2$y
        if(i!=j){
-         matriz[i,j]<-sqrt(dx^2+dy^2) #Calcular distancia euclidiana
+         matriz[i,j]<-sqrt(dx1^2+dy1^2) #Calcular distancia euclidiana
          d<-matriz[i,j]
+         d<-d-particulas[i,]$espesor-particulas[j,]$espesor
          if (d<umbral) 
          {
            if (particulas[i,]$espesor<dcc && particulas[j,]$espesor<dcc){ #uniion
@@ -76,27 +56,56 @@ for (tiempo in 1:tmax){
                particulas[j,]$lider<-particulas[i,]$lider
                particulas[i,]$estado<-"L"
              }
+             print(paste("pos inicial particula",i, tiempo))
              print("aglomerado")
            }
            
-          else if (particulas[i,]$espesor>dcc || particulas[j,]$espesor>dcc){ #repelen
+          else if (particulas[i,]$espesor>dcc | particulas[j,]$espesor>dcc){ #repelen
             print(particulas[i,]$dx)
-            particulas[i,]$dx=particulas[i,]$dx*(-particulas[i,]$dx)
-            particulas[i,]$dy=particulas[i,]$dy*(-particulas[i,]$dy)
-            particulas[j,]$dx=particulas[j,]$dx*(-particulas[j,]$dx)
-            particulas[j,]$dy=particulas[j,]$dy*(-particulas[j,]$dy)
-            print(particulas[i,]$dx)
+            particulas[i,]$dx<-particulas[i,]$dx*-1
+            particulas[i,]$dy<-particulas[i,]$dy*-1
+            particulas[j,]$dx<-particulas[j,]$dx*-1
+            particulas[j,]$dy<-particulas[j,]$dy*-1
+            print(paste("pos inicial particula",i,j, tiempo))
             print("repelen")
-           }
+            print(particulas[i,]$dx)
+          
+          }
+         
          }
+     
        }
-      }
-    }
-  }
+      
+       }
+  
+     }
+   }
 }  
 
+  #Movimiento Browniano
+  for(i in 1:n){
+    p<-particulas[i,]
+    
+    p$x<- p$x + p$dx #Movimiento en x
+    p$y<- p$y + p$dy #Movimiento en y
+    
 
-
+    
+    if (p$x > l) {
+      p$x <- p$x - l
+    }
+    if (p$y > l) {
+      p$y <- p$y - l
+    }
+    if (p$x < 0) {
+      p$x <- p$x + l
+    }
+    if (p$y < 0) {
+      p$y <- p$y + l
+    }
+    particulas[i,]<-p
+  }
+tabla=rbind(tabla,particulas)
 #FuerzaAtracción->function(){}#Calcular fuerzas de atracción de van der waals 
 #Función inversa de la distancia pero elevada a un exponente mayor de dos
   #A=0.1*10^-20 #constante de Hamaker
@@ -113,11 +122,12 @@ for (tiempo in 1:tmax){
 
   
 #Graficar la simulación
-ggplot()+
-#ggplot(data=particulas, aes(x = particulas$x, y= particulas$y, label=rownames(particulas))) +
+
+#ggplot()+
+ggplot(data=particulas, aes(x = particulas$x, y= particulas$y, label=rownames(particulas))) +
   geom_point(data=particulas, aes(x = particulas$x, y= particulas$y,size=particulas$rf), color="gray80")+
   geom_point(data=particulas, aes(x = particulas$x, y= particulas$y),color="gray29")+
-  #geom_label(aes(fill = factor(particulas$lider)), colour = "white", fontface = "bold")+
+  geom_label(aes(fill = factor(particulas$lider)), colour = "white", fontface = "bold")+
   scale_x_continuous(name="x",limits = c(0, l))+
   scale_y_continuous(name="y",limits = c(0, l))+
   scale_colour_manual(values=colores)+  
